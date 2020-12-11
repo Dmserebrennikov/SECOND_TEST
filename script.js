701a64cb74d7be14dfb2a94cddb57f5c543d8c4a
Dmitry Serebrennikov <dimafanya@gmail.com>
2020-12-11 17:27:12 +0200

    Fetch topic

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 'use strict';
 
-const desert = { type: 'pie' };
-let newLine = 'Hello World'
+async function getUsers(names) {
+  let logins = [];
+
+  for (let user of names) {
+    let login = fetch(`https://api.github.com/users/${user}`).then(
+      sucess => {
+        if (sucess.status == 200) {
+          return sucess.json();
+        } else {
+          return 'null';
+        }
+      },
+      error => {
+        return 'null';
+      },
+    );
+    logins.push(login);
+  }
+
+  let res = await Promise.all(logins);
+
+  return res;
+}
+
+console.log(getUsers(['iliakan', 'remy', 'no.such.users']));
