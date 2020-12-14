3cd7318fb8a4c034fec8a6ab97206a36f59b8a90
Dmitry Serebrennikov <dimafanya@gmail.com>
2020-12-14 16:02:56 +0200

    Proper code Fetch 1

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 'use strict';
 
 async function getUsers(names) {
-  let logins = [];
+  let jobs = [];
 
-  for (let user of names) {
-    let login = fetch(`https://api.github.com/users/${user}`).then(
-      sucess => {
-        if (sucess.status == 200) {
-          return sucess.json();
-        } else {
-          return 'null';
-        }
-      },
-      error => {
-        return 'null';
-      },
-    );
-    logins.push(login);
+  for (let name of names) {
+    let job = (async () => {
+      let response = await fetch(`https://api.github.com/users/${name}`);
+      if (response.status != 200) return null;
+      return await response.json();
+    })();
+    jobs.push(job);
   }
 
-  let res = await Promise.all(logins);
+  let results = await Promise.all(jobs);
 
-  return res;
+  return results;
 }
 
-console.log(getUsers(['iliakan', 'remy', 'no.such.users']));
+let x = getUsers(['iliakan', 'remy', 'no.such.users']);
+x.then(r => console.log(r));
